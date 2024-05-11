8f41fdc51f144a075452ac915f2eab84407addb0
ArsenHovhan <arsen.h12@tumo.org>
2024-05-11 13:28:54 +0400

    data

---
+++ AudioPlayer/script.js
@@ -0,0 +1,8 @@
+let data = {
+    title : ["Hit the Road Jack",
+             "Lose Control"],
+    song : ["music/Ray Charles Hit The Road Jack.mp3",
+            "music/Missy Elliott, Ciara, Fatman Scoop  Lose Control.mp3"],
+    poster: ["https://www.noviscore.com/sleeve255-hit-the-road-jack-ray-charles.jpg",
+             "https://i1.sndcdn.com/artworks-ycpcO77e5r7x5zwN-3zUn0w-t500x500.jpg"]
+}
